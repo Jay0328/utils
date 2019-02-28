7b03a9768131ce44c071b7c6d25dc74e4f4293d0
Jay <gcobs135102@gmail.com>
2019-03-01 00:08:20 +0800

    feat(validator): email

---
+++ src/validator/email.ts
@@ -0,0 +1,8 @@
+export type IsEmail = (value: string) => boolean;
+
+const isEmail: IsEmail = value => (
+	// tslint:disable-next-line:max-line-length
+	/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
+);
+
+export default isEmail;
