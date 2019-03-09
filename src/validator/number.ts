9d151c304c040458fff3211736ddced18ead2216
Jay <gcobs135102@gmail.com>
2019-03-09 16:49:17 +0800

    docs(validator): number

--- src/validator/number.ts
+++ src/validator/number.ts
@@ -1,9 +1,18 @@
-export type IsInteger = (number: number | string, signs?: '+-' | '+' | '-') => boolean;
-export const isInteger: IsInteger = (number, signs = '+-') => (
-	new RegExp(`^(0|[${signs}]?[1-9]\\d*)$`).test(`${number}`)
-);
+/**
+ * @module validator.number
+ */
 
-export type IsNumeric = (number: number | string) => boolean;
-export const isNumeric: IsNumeric = number => (
-	!Number.isNaN(parseFloat(`${number}`)) && isFinite(number as number)
-);
+/**
+ * @param arg argument will be tested if it is integer or not.
+ * @param signs 
+ */
+export function isInteger(arg: number | string, signs: '+-' | '+' | '-' = '+-'): boolean {
+	return new RegExp(`^(0|[${signs}]?[1-9]\\d*)$`).test(`${arg}`);
+}
+
+/**
+ * @param arg argument will be tested if it is numeric or not.
+ */
+export function isNumeric(arg: number | string): boolean {
+	return !Number.isNaN(parseFloat(`${arg}`)) && isFinite(arg as number);
+}
