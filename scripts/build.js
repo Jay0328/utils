19a747d02b7b986b56a8ae61dc0547540e6fd69a
Jay <gcobs135102@gmail.com>
2019-03-09 14:05:22 +0800

    build: umd

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,82 @@
 const path = require('path');
 const { rollup } = require('rollup');
-const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
+const typescript = require('rollup-plugin-typescript');
+const { uglify } = require('rollup-plugin-uglify');
 const entries = require('./entries');
 
-const build = async () => {
+const root = path.resolve(__dirname, '..');
+const onlyStripCommentsOptions = {
+	compress: false,
+	mangle: false,
+	output: {
+		comments: false,
+		beautify: true,
+	},
+};
+
+const buildCommonJS = async () => {
 	const input = await entries();
 	const plugins = [
-		resolve(),
 		babel({
 			exclude: 'node_modules/**',
-			extensions: ['.js', '.jsx', '.ts', '.tsx']
+			extensions: ['.ts', '.js']
 		}),
+		uglify(onlyStripCommentsOptions)
 	];
-	const bundle = await rollup({ input, plugins });
-	const dir = path.resolve(__dirname, '..');
+	const external = Object
+		.entries(input)
+		.map(([ignored, filePath]) => {
+			if (filePath.includes('index.ts')) {
+				return filePath.replace('/index.ts', '');
+			}
+
+			return filePath.replace('.ts', '');
+		});
+	const bundle = await rollup({ input, plugins, external });
+	const dir = root;
 
 	await bundle.write({
 		dir,
 		format: 'cjs',
-		sourcemap: false
+		sourcemap: false,
+		interop: false,
 	});
 };
+
+const buildUMD = async () => {
+	const input = path.resolve(root, 'src', 'index.ts');
+	const name = 'Utils';
+	const buildConfigs = [
+		{
+			file: './umd/utils.development.js',
+			uglifyOptions: onlyStripCommentsOptions
+		},
+		{
+			file: './umd/utils.production.min.js',
+		}
+	];
+	const write = async ({ file, uglifyOptions }) =>
+		(await rollup({
+			input,
+			plugins: [
+				typescript(),
+				uglify(uglifyOptions)
+			]
+		})).write({
+			file,
+			format: 'umd',
+			sourcemap: false,
+			interop: false,
+			name
+		});
+
+	await Promise.all(buildConfigs.map(write));
+};
+
+const build = async () => await Promise.all([
+	buildCommonJS(),
+	buildUMD()
+]);
 
 build();
--- scripts/build.remove.js
+++ scripts/build.remove.js
@@ -4,18 +4,32 @@ const { exec } = require('child_process');
 const root = path.resolve(__dirname, '..');
 const src = path.resolve(root, 'src');
 
-const removeBuild = async () => await Promise.all(
-	await fs
-		.readdirSync(path.resolve(src))
-		.map(async dir => {
-			const stat = await fs.statSync(path.resolve(src, dir));
+const removeCommonJS = async () => {
+	await Promise.all(
+		await fs
+			.readdirSync(path.resolve(src))
+			.map(async dir => {
+				const stat = await fs.statSync(path.resolve(src, dir));
 
-			if (stat.isDirectory()) {
-				return exec(`rm -rf ${path.resolve(root, dir)}`)
-			}
+				if (stat.isDirectory()) {
+					return exec(`rm -rf ${path.resolve(root, dir)}`)
+				}
 
-			return;
-		})
-);
+				return;
+			})
+	);
+
+	await exec(`rm ${path.resolve(root, 'index.js')}`);
+	await exec(`rm ${path.resolve(root, 'index.d.ts')}`);
+};
+
+const removeUMD = async () => {
+	await exec(`rm -rf ${path.resolve(root, 'umd')}`);
+};
+
+const removeBuild = async () => await Promise.all([
+	removeCommonJS(),
+	removeUMD()
+]);
 
 removeBuild();
