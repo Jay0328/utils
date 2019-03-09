2fc21a7442ce77183704b587f8b9e1f7a5321dbf
Jay <gcobs135102@gmail.com>
2019-03-09 21:47:34 +0800

    build: add entry for umd

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as constant from './constant';
+import * as helper from './helper';
+import * as parser from './parser';
+import * as validator from './validator';
+
+export {
+	constant,
+	helper,
+	parser,
+	validator,
+};
---
+++ src/validator/index.ts
@@ -0,0 +1,5 @@
+export { isArray, isEmptyArray } from './array';
+export { default as isEmail } from './email';
+export { isInteger, isNumeric } from './number';
+export { isObject, isEmptyObject } from './object';
+export { default as isUrl } from './url';
