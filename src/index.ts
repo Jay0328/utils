eeefc22a087ec7f95051615afcb993bd74c7b5ce
Jay <gcobs135102@gmail.com>
2019-03-18 02:19:41 +0800

    feat(comparator): shallowEqual
    from react

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import * as comparator from './comparator';
 import * as constant from './constant';
 import * as helper from './helper';
 import * as parser from './parser';
 import * as validator from './validator';
 
 export {
+	comparator,
 	constant,
 	helper,
 	parser,
