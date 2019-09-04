2ba778e2228cf03d40c93e72edabbd132757842e
Jay Chen <gcobs135102@gmail.com>
2019-09-05 02:14:33 +0800

    fix: return NaN if any parameter in formatPrecision is not number

--- src/formatPrecision.ts
+++ src/formatPrecision.ts
@@ -1,12 +1,16 @@
 /**
  * @param arg source number.
  * @param precisions length of precisions.
- * 
+ *
  * ```ts
  * formatPrecision(1.1234, 2) === 1.12
  * ```
  */
-export function formatPrecision(arg: number, precisions: number): number {
+export function formatPrecision(arg: number, precisions: number) {
+  if (typeof arg !== 'number' || typeof precisions !== 'number') {
+    return NaN;
+  }
+
   const size = 10 ** precisions;
   return Math.round(arg * size) / size;
 }
