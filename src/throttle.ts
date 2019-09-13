08cc9fba340a2261ca45393c749791553bf4fa81
Jay Chen <gcobs135102@gmail.com>
2019-09-13 10:59:07 +0800

    feat: throttle factory and Throttle decorator

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,32 +1,22 @@
 /**
- * Add throttle to a function.
+ * A factory to create throttled function.
  * 
- * @typeparam T type or interface of origin function.
+ * @typeparam F type or interface of origin function.
+ * 
  * @param time debounce time.
- * @returns The behavior is the same as origin function, but with throttle.
  * 
- * ```ts
+ * @returns The behavior is the same as origin function, but throttled.
+ * 
+ * @example
+ * 
  * const origin = () => ...;
  * const result = throttle(250)(origin);
- * ```
- * 
- * ```ts
- * class Greeter {
- * 	greeting: string;
- * 	constructor(message: string) {
- * 		this.greeting = message;
- * 	}
- * 
- * 	@throttle(500)
- * 	greet = () => `Hello, ${this.greeting}`;
- * }
- * ```
  */
-export function throttle<T extends (...args: any[]) => any>(time: number) {
-  return (fn: T) => {
+export function throttle(time: number) {
+  return <F extends (...args: any[]) => void>(fn: F) => {
     let previous: number;
 
-    return function (...args: Parameters<T>): any {
+    return function (...args: Parameters<F>): void {
       // @ts-ignore
       const context = this;
       const now = +(new Date());
@@ -36,5 +26,42 @@ export function throttle<T extends (...args: any[]) => any>(time: number) {
         previous = now;
       }
     };
+  };
+}
+
+/**
+ * A decorator to create throttled instance method.
+ * 
+ * @typeparam F type or interface of origin function.
+ * 
+ * @param time debounce time.
+ * 
+ * @returns The behavior is the same as origin function, but throttled.
+ * 
+ * @example
+ * 
+ * class Greeter {
+ * 	greeting: string;
+ * 	constructor(message: string) {
+ * 		this.greeting = message;
+ * 	}
+ * 
+ * 	@Throttle(500)
+ * 	greet = () => `Hello, ${this.greeting}`;
+ * }
+ */
+export function Throttle(time: number) {
+  return function <F extends (...args: any[]) => void>(
+    target: any,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<F>
+  ) {
+    const originalMethod = descriptor.value;
+
+    if (originalMethod) {
+      descriptor.value = throttle(time)(originalMethod) as F;
+    }
+
+    return descriptor;
   };
 }
